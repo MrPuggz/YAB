b6821fc033e0009d725ee3074576a188dc8a8ec8
MrPuggz <43212434+MrPuggz@users.noreply.github.com>
2018-09-14 15:01:11 +0100

    Create YAB.py

---
+++ YAB.py
@@ -0,0 +1,24 @@
+
+import discord
+import asyncio
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello {0.author.mention}'.format(message))
+
+                
+
+client = MyClient()
+
+client.run('NDg2MTQ5MzIwOTM3MTc3MTA4.DnqSDg.2CrJybgChovknJRNbmWTIpEkJtY')
